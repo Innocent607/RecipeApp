1cc70858ba2c08c94964c84fc0da3ac0aaa01c3d
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-12-13 09:00:47 +0200

    1st commit - Spring MVC CRUD Operations

---
+++ src/main/java/com/inno/RecipeApp/model/Recipe.java
@@ -0,0 +1,31 @@
+package com.inno.RecipeApp.model;
+
+
+public class Recipe {
+
+    private String recipeName;
+    private long id;
+
+    public Recipe() {
+    }
+
+    public Recipe(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
